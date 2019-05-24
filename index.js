b19d35c291ef5bd91915d57e74bf3674c87fff60
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-24 16:25:58 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const {Client, SQLiteProvider} = require('awesome-commando');
+const sqlite = require('sqlite');
+
+require('dotenv').config({path: path.join(__dirname, 'data/.env')});
+
+const client = new Client({
+  owner: '147800635046232064',
+  commandPrefix: process.env.prefix,
+  disableEveryone: true,
+  unknownCommandResponse: false,
+  selfbot: false
+});
+
+sqlite.open(path.join(__dirname, 'settings.sqlite3')).then((db) => {
+  client.setProvider(new SQLiteProvider(db));
+});
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['test', 'Test Commands']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    help: true,
+    prefix: true,
+    ping: true,
+    eval_: true,
+    commandState: true
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.on('ready', () => {
+  console.log('Logged in!');
+});
+ 
+client.on('error', console.error);
+
+
+client.login(process.env.TOKEN);
