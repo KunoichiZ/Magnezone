92a3ab6a765e3996a6f11cf48940b61c4d8aa29f
Kaoru <KunoichiZ@users.noreply.github.com>
2019-05-31 17:05:28 -0500

    Add more command files

---
+++ commands/pokemon/ability.js
@@ -0,0 +1,49 @@
+const {Command} = require('awesome-commando');
+import { MessageEmbed } from 'awesome-djs';
+import Fuse from 'fuse.js';
+let abilities = JSON.parse(fs.readFileSync("./data/ability.json", "utf8"))
+
+var options = {
+    shouldSort: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+      "name"
+    ]
+  };
+  
+
+module.exports = class AbilityCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ability',
+      group: 'pokemon',
+      memberName: 'ability',
+      description: 'Replies with a description of a Pokemon\'s ability.',
+      examples: ['ability Aftermath'],
+      args: [
+        {
+            key: 'ability',
+            prompt: 'What ability do you want more info on?',
+            type: 'string'
+        }
+    ]
+    });
+  }
+
+  run (msg) {
+    var fuse = new Fuse(abilities, options); // "list" is the item array
+    var result = fuse.search(ability);
+    console.log(result)
+    // const abilityEmbed = new MessageEmbed()
+    //     .setColor(msg.member.displayHexColor)
+    //     .setDescription(result)
+    //     // .setDescription(`${args.member.user.username}, you've been poked by ${msg.author.username}!`)
+    //     // .setImage(imgURLs[randomNum])
+    // msg.channel.send(abilityEmbed);
+    // return msg.say('Hi, I\'m awake!');
+  }
+};
