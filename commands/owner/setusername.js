82cb0a1bdf3f2d3249b784b37c15ea6f2fa482ba
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-02 00:03:13 -0500

    Update setusername.js

---
+++ commands/owner/setusername.js
@@ -0,0 +1,35 @@
+const {Command} = require('awesome-commando');
+
+module.exports = class SetUsernameCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'setusername',
+      aliases: ['setun'],
+      group: 'owner',
+      memberName: 'setusername',
+      description: 'Sets the username of the bot.',
+      examples: ['setusername'],
+      guildOnly: true,
+      ownerOnly: true,
+      args: [
+        {
+          key: 'username',
+          prompt: 'What username to set?',
+          type: 'string',
+          default: 'Fuguri'
+        }
+      ]
+    });
+  }
+
+  async run (msg, {username}) {
+
+    try {
+      const user = await this.client.user.setUsername(username);
+      
+      return msg.embed({description: `My new username is ${user.username}`});
+    } catch (err) {
+      return console.error(err);
+    }
+  }
+};
