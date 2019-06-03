96035be11cab30a46362b834844e7d908293602c
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-02 22:12:34 -0500

    Update ban.js

---
+++ commands/moderation/ban.js
@@ -0,0 +1,43 @@
+const {Command} = require('awesome-commando');
+
+module.exports = class ReplyCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ban',
+      aliases: ['b'],
+      group: 'moderation',
+      memberName: 'ban',
+      description: 'Bans a member from the server',
+      examples: ['ban AinoAlt'],
+      args: [
+        {
+            key: 'member',
+            prompt: 'Which member should I ban?',
+            type: 'member',
+        },
+        {
+            key: 'reason',
+            prompt: 'What is the reason for this banishment?',
+            type: 'string',
+            default: '',
+        }
+    ],
+    });
+  }
+
+  run (msg, { member }, { reason }) {
+    if (member.id === msg.author.id) return msg.reply('I don\'t think you want to ban yourself.');
+    if (!member.bannable) return msg.reply('I cannot ban that member, their role is probably higher than my own!');
+    const user = msg.mentions.users.first();
+    msg.guild.ban(user);
+
+    const banEmbed = new MessageEmbed()
+        .setColor(msg.member.displayHexColor)
+        .setDescription(`
+          **Member:** ${member.user.tag} (${member.id})
+          **Action:** Ban
+          **Reason:** ${reason !== '' ? reason : 'No reason given by staff'}`
+        )
+    return msg.channel.send(banEmbed);
+  }
+};
