cab0be4ae1e61385308a78f2f30bcd43f463f02c
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-04 23:25:05 -0500

    Update about.js

--- commands/info/about.js
+++ commands/info/about.js
@@ -1,7 +1,7 @@
 const { Command } = require('awesome-commando');
 const { MessageEmbed } = require('awesome-djs');
 const moment = require('moment');
-require("moment-duration-format");
+require('moment-duration-format');
 
 module.exports = class AboutCommand extends Command {
   constructor (client) {
@@ -20,7 +20,7 @@ module.exports = class AboutCommand extends Command {
         .setColor(msg.member.displayHexColor)
         .setAuthor(`${this.client.user.username} Stats`, this.client.user.displayAvatarURL({ format: 'png' }))
         .addField('Owner', this.client.owners[0].tag, true)
-        .addField('Uptime', `I've been online for ${moment.duration(process.uptime()).format('DD [days], HH [hours, ] mm [minutes and] ss [seconds]')}`)
+        .addField('Uptime', `I've been online for ${moment.duration(process.uptime() * 1000).format('DD [days], HH [hours, ] mm [minutes and] ss [seconds]')}`)
         .addField('License', 'GPL-3.0')
         .addField('Source Code', 'https://github.com/KunoichiZ/Magnezone')
         .addField('Documentation', 'https://www.magnezone.kunoichiz.me/');
