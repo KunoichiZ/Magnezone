211ab1f2a26552876a66049d07e643164cc71b2c
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-04 23:46:39 -0500

    Update lockdown and unlock commands

--- commands/moderation/lockdown.js
+++ commands/moderation/lockdown.js
@@ -16,7 +16,40 @@ module.exports = class LockdownCommand extends Command {
     });
   }
 
-  run (msg) {
-    
+  async run (msg) {
+    const lockEmbed = new MessageEmbed();
+    const modlogsChannel = this.client.channels.get('585656872355364864');
+    const overwrite = await msg.channel.overwritePermissions({
+      permissionOverwrites: [
+          {
+              allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+              id: msg.member.roles.highest.id
+          },
+          {
+              deny: ['SEND_MESSAGES'],
+              id: msg.guild.defaultRole.id
+          }
+      ],
+      reason: 'Channel Lockdown',
+    });
+
+    msg.channel.overwritePermissions(msg.guild.id, {
+      SEND_MESSAGES: false
+    })
+
+    lockEmbed
+        .setColor(msg.member.displayHexColor)
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+        .setDescription(`**Action:** 🔒 locked the \`${msg.channel.name}\` channel.\n
+            **Details:** Only staff can now access this channel. Use \`${msg.guild.commandPrefix}unlock\` in this channel to unlock the channel`)
+        .setTimestamp();
+
+    modlogsChannel.send(lockEmbed);
+
+    if (msg.deletable) {
+      msg.delete();
+    }
+
+    return msg.channel.send(lockEmbed);
   }
 };
--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -12,11 +12,48 @@ module.exports = class UnlockCommand extends Command {
       guildOnly: true,
       userPermissions: ['MANAGE_CHANNELS'],
       clientPermissions: ['MANAGE_CHANNELS'],
-      examples: ['unlock']
+      examples: ['unlock'],
+      args: [
+        {
+            key: 'lockrole',
+            prompt: 'Which role to apply the lockdown to?',
+            type: 'role',
+            default: 'everyone',
+        }
+      ]
     });
   }
 
-  run (msg) {
-    
+  async run (msg) {
+    const unlockEmbed = new MessageEmbed();
+    const modlogsChannel = this.client.channels.get('585656872355364864');
+    const overwrite = await msg.channel.overwritePermissions({
+      permissionOverwrites: [
+        {
+          allow: ['SEND_MESSAGES'],
+          id: msg.guild.defaultRole.id,
+      }
+      ],
+      reason: 'Channel Lockdown',
+    });
+
+    msg.channel.overwritePermissions(msg.guild.id, {
+      SEND_MESSAGES: true
+    })
+
+    unlockEmbed
+        .setColor(msg.member.displayHexColor)
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+        .setDescription(`**Action:** 🔓 unlocked the \`${msg.channel.name}\` channel.
+        **Details:** This channel can now be used by everyone again. Use \`${msg.guild.commandPrefix}lockdown\` in this channel to (re)-lock it.`)
+        .setTimestamp();
+
+    modlogsChannel.send(unlockEmbed);
+
+    if (msg.deletable) {
+      msg.delete();
+    }
+
+    return msg.channel.send(unlockEmbed);
   }
 };
