607191be2ecb77c3f77eec7cb9542666ad4c4bf9
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-07 22:33:09 -0500

    Add nickname to owner field

--- commands/info/about.js
+++ commands/info/about.js
@@ -16,10 +16,11 @@ module.exports = class AboutCommand extends Command {
   }
 
   run (msg) {
+    const nickname = msg.guild.members.get("147800635046232064").nickname;
     const aboutEmbed = new MessageEmbed()
         .setColor(msg.member.displayHexColor)
         .setAuthor(`${this.client.user.username} Stats`, this.client.user.displayAvatarURL({ format: 'png' }))
-        .addField('Owner', this.client.owners[0].tag, true)
+        .addField('Owner', this.client.owners[0].tag + ' (' + nickname + ')', true)
         .addField('Uptime', moment.duration(process.uptime() * 1000).format('D [days], H [hours] [and] m [minutes]'))
         .addField('License', 'GPL-3.0')
         .addField('Source Code', 'https://github.com/KunoichiZ/Magnezone')
