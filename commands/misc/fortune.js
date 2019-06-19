af87b7e26944685c15c33e75011f63ed63230cc4
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-19 16:21:51 -0500

    Add fortune cookie command

---
+++ commands/misc/fortune.js
@@ -0,0 +1,41 @@
+const { Command } = require('awesome-commando');
+const { MessageEmbed } = require('awesome-djs');
+const Fuse = require('fuse.js');
+var fs = require("fs");
+let fortunes = JSON.parse(fs.readFileSync("./data/fortunes.json", "utf8"))
+
+var fortuneOptions = {
+    keys: ['id']
+}
+
+module.exports = class FortuneCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'fortune',
+      group: 'misc',
+      aliases: ['cookie'],
+      memberName: 'fortune',
+      description: 'Displays a random fortune',
+      examples: ['fortune', 'cookie'],
+      guildOnly: true
+    });
+  }
+
+  run (msg) {
+    const fortune = this.client.emojis.find(emoji => emoji.name === "fortune");
+    var random = Math.floor(Math.random() * 54) + 1;
+    var randomTxt = random.toString();
+    var fortunesFuse = new Fuse(fortunes.fortunes, fortuneOptions); 
+    var result = fortunesFuse.search(randomTxt);
+    const fortuneEmbed = new MessageEmbed();
+    // console.log(randomTxt)
+    // console.log(result[0].msg)
+    
+    
+    fortuneEmbed
+        .setColor('#ffff00')
+        .addField(`${fortune} Fortune`, result[0].msg)
+    
+    return msg.channel.send(fortuneEmbed);
+  }
+};
